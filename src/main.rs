94b7a51fea8a0d5ae95400fc9e2895c2d8889202
ahaoboy <504595380@qq.com>
2024-09-26 21:32:12 +0800

    add qjs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use rquickjs::{Context, Runtime};
+
+pub fn add(a: i32, b: i32) -> i32 {
+    let rt = Runtime::new().unwrap();
+    let ctx = Context::full(&rt).unwrap();
+
+    ctx.with(|ctx| -> i32 {
+        let ret = ctx.eval::<i32, _>(format!("{}+{}", a, b)).unwrap();
+        ret
+    })
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! {}", add(1, 2));
 }
